bb3552fdc9ea45681b8a99f7f09501bb08edc840
Jordan Koehler <digipower66@gmail.com>
2020-12-28 16:09:12 -0500

    Current countdown starts on page load

--- index.js
+++ index.js
@@ -23,4 +23,18 @@ function setTime() {
   }, 1000);
 }
 
-setTime();
+function init() {
+  const now = Date.parse(new Date());
+  const timeLeft = newYear - now;
+  let seconds = Math.floor((timeLeft / 1000) % 60);
+  let minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+  let hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
+  setTime();
+}
+init();
