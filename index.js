a2f7d6331498b5e6bca8b1391ecb561110a5330e
Jordan Koehler <digipower66@gmail.com>
2020-12-29 16:57:25 -0500

    add 0 to numbers less than 2 digits, edit CSS

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const hour = document.getElementById("hours");
 const minute = document.getElementById("minutes");
 const second = document.getElementById("seconds");
 
+function add0(num) {
+  return ("0" + num).slice(-2);
+}
+
 function setTime() {
   setInterval(function () {
     const now = Date.parse(new Date());
     const timeLeft = newYear - now;
-    let seconds = Math.floor((timeLeft / 1000) % 60);
-    let minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-    let hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-
+    let seconds = add0(Math.floor((timeLeft / 1000) % 60));
+    let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
+    let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
+    let days = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
     day.innerHTML = days;
     hour.innerHTML = hours;
     minute.innerHTML = minutes;
@@ -26,11 +29,10 @@ function setTime() {
 function init() {
   const now = Date.parse(new Date());
   const timeLeft = newYear - now;
-  let seconds = Math.floor((timeLeft / 1000) % 60);
-  let minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-  let hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-
+  let seconds = add0(Math.floor((timeLeft / 1000) % 60));
+  let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
+  let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
+  let days = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
   day.innerHTML = days;
   hour.innerHTML = hours;
   minute.innerHTML = minutes;
