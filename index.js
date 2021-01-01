94297ab819434f9ac260d3bf05ff06d5b6f9b063
Jordan Koehler <digipower66@gmail.com>
2020-12-31 22:28:02 -0500

    change countdown to July 2025

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const newYear = Date.parse(new Date().getFullYear() + 1, 0, 1);
+const dateCountdown = new Date("July 19, 2025");
 let time;
 
 //DOM
@@ -6,29 +6,41 @@ const day = document.getElementById("days");
 const hour = document.getElementById("hours");
 const minute = document.getElementById("minutes");
 const second = document.getElementById("seconds");
+const topAnimate = document.querySelector(".top");
+const bottom = document.querySelector(".bottom");
 
 function add0(num) {
   return ("0" + num).slice(-2);
 }
 
+function animateFlip(num) {
+  if (num == num) {
+    // topAnimate.style.rotationX = "-180deg";
+    topAnimate.style.color = "orangered";
+    console.log(num);
+  }
+}
+
 function setTime() {
   setInterval(function () {
     const now = Date.parse(new Date());
-    const timeLeft = newYear - now;
+    const timeLeft = dateCountdown - now;
     let seconds = add0(Math.floor((timeLeft / 1000) % 60));
     let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
     let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
     let days = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+
     day.innerHTML = days;
     hour.innerHTML = hours;
     minute.innerHTML = minutes;
     second.innerHTML = seconds;
   }, 1000);
+  animateFlip(seconds);
 }
 
 function init() {
   const now = Date.parse(new Date());
-  const timeLeft = newYear - now;
+  const timeLeft = dateCountdown - now;
   let seconds = add0(Math.floor((timeLeft / 1000) % 60));
   let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
   let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
