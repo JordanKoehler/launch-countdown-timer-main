af2c9d892ade7b6a886e40d7776cea5e5f17cdf3
JordanKoehler <digipower66@gmail.com>
2021-01-02 00:06:20 -0500

    animation now checks if the number changed to play

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const dateCountdown = new Date("January 2 2021");
-
 //DOM
 const day = document.getElementById("days");
 const hour = document.getElementById("hours");
@@ -11,48 +9,60 @@ function add0(num) {
   return ("0" + num).slice(-2);
 }
 
-function animateFlip(num, section) {
+function animateFlip(num, newNum, section) {
   const topAnimate = document.querySelector("." + section + " .top");
-
-  topAnimate.classList.add("animationClass");
-  setTimeout(function () {
-    topAnimate.classList.remove("animationClass");
-  }, 500);
+  if (num != newNum) {
+    topAnimate.classList.add("animationClass");
+    setTimeout(function () {
+      topAnimate.classList.remove("animationClass");
+    }, 500);
+  }
 }
+const time = {
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+  days: 0,
+  countdown: new Date("January 3,2021"),
+};
 
-function setTime() {
+function init() {
   setInterval(function () {
     const now = Date.parse(new Date());
-    const timeLeft = dateCountdown - now;
+    const timeLeft = time.countdown - now;
 
-    //Grabbing new countdown numbers and store
-    let seconds = add0(Math.floor((timeLeft / 1000) % 60));
-    let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
-    let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
-    let days = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
-
+    day.innerHTML = days;
+    hour.innerHTML = hours;
+    minute.innerHTML = minutes;
     second.innerHTML = seconds;
-    animateFlip(days, "days");
-    animateFlip(seconds, "seconds");
-    animateFlip(minutes, "minutes");
-    animateFlip(hours, "hours");
+
+    function setTime() {
+      //Grabbing new countdown numbers and store
+      newSeconds = add0(Math.floor((timeLeft / 1000) % 60));
+      newMinutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
+      newHours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
+      newDays = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
+
+      second.innerHTML = seconds;
+
+      day.innerHTML = newDays;
+      hour.innerHTML = newHours;
+      minute.innerHTML = newMinutes;
+      second.innerHTML = newSeconds;
+
+      animateFlip(time.days, newDays, "days");
+      animateFlip(time.seconds, newSeconds, "seconds");
+      animateFlip(time.minutes, newMinutes, "minutes");
+      animateFlip(time.hours, newHours, "hours");
+      console.log(time.seconds, newSeconds);
+
+      time.seconds = newSeconds;
+      time.minutes = newMinutes;
+      time.hours = newHours;
+      time.days = newDays;
+    }
+    setTime();
   }, 1000);
 }
 
-function init() {
-  const now = Date.parse(new Date());
-  const timeLeft = dateCountdown - now;
-  let seconds = add0(Math.floor((timeLeft / 1000) % 60));
-  let minutes = add0(Math.floor((timeLeft / 1000 / 60) % 60));
-  let hours = add0(Math.floor((timeLeft / (1000 * 60 * 60)) % 24));
-  let days = add0(Math.floor(timeLeft / (1000 * 60 * 60 * 24)));
-
-  let prevSec = seconds;
-  day.innerHTML = days;
-  hour.innerHTML = hours;
-  minute.innerHTML = minutes;
-  second.innerHTML = seconds;
-  animateFlip(seconds, "seconds");
-  setTime();
-}
 init();
